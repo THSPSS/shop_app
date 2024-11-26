cb6a65789f18bba3acce5d77ca7f065fb22ac675
Seonyoung Pang <sunyeongbang98@gmail.com>
2024-11-26 19:52:52 +0900

    update title and search textfield section

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,30 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-            Text('Shoes', style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 35.0
-              )),
-            Text('Collection', style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 35.0
-              )),
-            TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.only(topRight: Radius.circular(10.0) , bottomRight: Radius.circular(10.0)),)
-                      ,hintText: 'Search',
-                      prefixIcon: Icon(Icons.search)
+          const SizedBox(
+            height: 140.0,
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Shoes\nCollection',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 35.0)),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black54),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30.0),
+                              bottomLeft: Radius.circular(30.0)),
+                        ),
+                        hintText: 'Search',
+                        prefixIcon: Icon(Icons.search)),
                   ),
                 ),
-      ],)
-    );
+              ],
+            ),
+          ),
+          //categories
+          //using listviewBuilder? or just using for loop?
+          Flexible(
+            child: SizedBox(
+              height: 60.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (BuildContext context, int i) {
+                  return TextButton(onPressed: null, child: Text('button $i'));
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
