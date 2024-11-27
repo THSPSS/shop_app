b51a38e64a9a8319483504612e78b7d0516ccccc
Seonyoung Pang <sunyeongbang98@gmail.com>
2024-11-27 16:56:02 +0900

    update main home page with filter button and products container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,37 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:shop_app_flutter/gloabl_variable.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const [
+    'All',
+    'Adidas',
+    'Nike',
+    'Puma',
+    'Reebok',
+    'Under Armour'
+  ];
+  String? selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(30.0)),
+    );
+
     return Scaffold(
         body: SafeArea(
       child: Column(
@@ -24,32 +49,82 @@ class HomePage extends StatelessWidget {
                 ),
                 Expanded(
                   child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black54),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30.0),
-                              bottomLeft: Radius.circular(30.0)),
-                        ),
-                        hintText: 'Search',
-                        prefixIcon: Icon(Icons.search)),
-                  ),
+                      decoration: InputDecoration(
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                  )),
                 ),
               ],
             ),
           ),
           //categories
           //using listviewBuilder? or just using for loop?
-          Flexible(
-            child: SizedBox(
-              height: 60.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (BuildContext context, int i) {
-                  return TextButton(onPressed: null, child: Text('button $i'));
-                },
-              ),
+          SizedBox(
+            height: 60.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, i) {
+                final filter = filters[i];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter;
+                      });
+                    },
+                    child: Chip(
+                      label: Text(filter),
+                      labelStyle: const TextStyle(
+                        fontSize: 16.0,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      shape: const RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      backgroundColor: selectedFilter == filter
+                          ? Theme.of(context).colorScheme.primary
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, i) {
+                return Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green.withOpacity(0.6),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(products[i]['title'].toString(),
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      Text('\$${products[i]["price"].toString()}',
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      Image.asset(products[i]['imageUrl'].toString())
+                    ],
+                  ),
+                );
+              },
             ),
           )
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shopping App',
       theme: ThemeData(
-        fontFamily: 'Lato',
-        //color scheme is based on seed Color, which will be base color of app
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(254, 206, 1, 1))
-        ),
-      home: const HomePage() ,
+          fontFamily: 'Lato',
+          //color scheme is based on seed Color, which will be base color of app
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(254, 206, 1, 1),
+            primary: const Color.fromRGBO(254, 206, 1, 1),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+              hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              prefixIconColor: Color.fromRGBO(119, 119, 119, 1))),
+      home: const HomePage(),
     );
   }
 }
